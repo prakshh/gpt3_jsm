c3e22baa7d05328ada6367a4db6118a441c69ba9
“prakshh” <prakashdas18@gmail.com>
2021-12-24 15:33:23 +0530

    exporting components and containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+// import Article from './components/Article/Article'
+// import Brand from './components/Brand/Brand'
+// import CTA from './components/CTA/CTA';
+// import Feature from './components/Feature/Feature';
+// import Navbar from './components/Navbar/Navbar';
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 const App = () => {
     return (
-        <div>
-            <h1>GPT-3</h1>
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
